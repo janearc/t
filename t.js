cad84a385c6b04d21fccbb216f3eed8f8a8157ab
jane arc <jane@cpan.org>
2015-01-04 15:44:51 +0700

    incremental

--- t.js
+++ t.js
@@ -6,18 +6,18 @@ var T = require( 'singleton' ).get();
 
 T.builtins = { };
 
-require( './lib/stdio'      ).init();
-require( './lib/list'       ).init();
-require( './lib/tokeparser' ).init();
-require( './lib/domain'     ).init();
-require( './lib/cli'        ).init();
+require( './lib/stdio'       ).init();
+require( './lib/list'        ).init();
+require( './lib/tokeparser'  ).init();
+require( './lib/domain'      ).init();
+require( './lib/diag'        ).init();
+require( './lib/lexer'       ).init();
+require( './lib/interpreter' ).init();
+require( './lib/cli'         ).init();
 
 // Set up the T machine
 //
 T.heap      = { };
-
-// Functions expected to exist in T without definition there
-//
 
 // We hit something we really didn't expect, and should get out right away.
 //
@@ -29,11 +29,11 @@ T.builtins.die = function () { process.exit(-255) },
 // should eventually be some kind of grown-up cleanup process but that works
 // for now.
 //
-T.builtins.bye = function () { while (respin()) { /* NOP */ } process.exit(0) },
+T.builtins.bye = function () { while (respin()) { /* NOP */ } process.exit(0) }
 
 // T needs to be able to read JSON.
 //
-T.builtins.jsonp = function (s) { return JSON.stringify( s, null, 2 ) },
+T.builtins.jsonp = function (s) { return JSON.stringify( s, null, 2 ) }
 
 // The environment for T
 //
@@ -59,7 +59,4 @@ function arg_sanitise () {
 	return process.argv;
 }
 
-T.target.contents.toString().split( "\n" ).forEach( function (line) {
-	var tokens = T.tokeparser( line );
-	console.log( tokens );
-} );
+// T.run( T.lex() );
--- test/tokeparser.js
+++
@@ -1,5 +0,0 @@
-parser = require( '../lib/tokeparser.js' ).tokeparser;
-
-console.log( parser( 'foo bar baz   bletch' ) );
-console.log( parser( 'foo bar "baz bletch" qip' ) );
-console.log( parser( 'foo bar' + "\n" + '  and baz bletch' ) );
